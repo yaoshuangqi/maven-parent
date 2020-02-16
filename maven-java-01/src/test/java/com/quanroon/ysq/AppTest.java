a74827bd1516f747cf7a5f01e6d31fb6d4061d6e
yaoshuangqi <1575848467@qq.com>
2020-02-16 21:11:08 +0800

    Initial commit

---
+++ maven-java-01/src/test/java/com/quanroon/ysq/AppTest.java
@@ -0,0 +1,20 @@
+package com.quanroon.ysq;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
