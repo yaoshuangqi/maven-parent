b5ea9134151f74abc90c249b403a1c755ac207a2
yaoshuangqi <yaosq@quanroon.com>
2020-09-24 10:07:29 +0800

    增加genertor插件，带lomback插件的比较实用；

--- maven-web-02/src/test/com/ysq/async/TaskTest.java
+++ maven-web-02/src/test/com/ysq/async/TaskTest.java
@@ -1,6 +1,7 @@
 package com.ysq.async;
 
 import com.ysq.Web2Application;
+import org.assertj.core.util.Lists;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.sound.midi.Soundbank;
+import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.Future;
 
 import static org.junit.Assert.*;
@@ -29,11 +33,40 @@ public class TaskTest {
 
     @Test
     public void doTaskOne() throws Exception{
-        long start = System.currentTimeMillis();
+//        long start = System.currentTimeMillis();
+//
+//        task.doTaskOne();
+//        task.doTaskTwo();
+//
+//        Thread.currentThread().join();
 
-        task.doTaskOne();
-        task.doTaskTwo();
+        String test = null;
 
-        Thread.currentThread().join();
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        Object[] objects = {new TaskTest(), "gg"};
+        test(objects);
+        List<String> officeIds = Lists.newArrayList();
+        officeIds.add("323233212");
+        officeIds.add("Defge232");
+        String join = String.join(",", officeIds);
+
+        System.out.println(join);
+
+    }
+    private static void test(Object... args){
+
+        Object[] params = args;
+
+        TaskTest dd = null;
+
+        try {
+            dd = (TaskTest)params[0];
+            System.out.println(dd.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
